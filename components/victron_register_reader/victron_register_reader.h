831962dcb5aa574b84d5ad664a1f93d17ff4188f
PPolo11 <124139395+PPolo11@users.noreply.github.com>
2026-04-05 19:05:23 +0200

    Update victron_register_reader.h

--- components/victron_register_reader/victron_register_reader.h
+++ components/victron_register_reader/victron_register_reader.h
@@ -2,22 +2,28 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/sensor/sensor.h"
+#include "esphome/components/victron_ble/victron_ble.h"
 #include <string>
 
 namespace esphome {
 namespace victron_register_reader {
 
-class VictronRegisterReader : public PollingComponent, public sensor::Sensor {
+class VictronRegisterReader : public PollingComponent,
+                              public sensor::Sensor,
+                              public victron_ble::VictronBLEListener {
  public:
   VictronRegisterReader(const std::string &mac, uint16_t reg, uint32_t update_interval);
 
   void update() override;
 
+  // BLE Callback
+  void on_solar_charger_data(const victron_ble::SolarChargerData &data) override;
+
  protected:
   std::string mac_address_;
   uint16_t register_;
+  float last_value_ = NAN;
 };
 
 }  // namespace victron_register_reader
 }  // namespace esphome
-
